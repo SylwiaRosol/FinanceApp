3fa759521be9bf8893972b22ab82cdffb524f3cc
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2021-02-14 14:19:23 +0100

    Balance

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 
-function goToTheMenu() {
-    window.location.href = 'Menu.html'; 
-    return false; 
-}
 
+$('#dropdown03').click(function() {
 
-$('#signIn').on('click', goToTheMenu);
-$('#goToTheMenu').on('click', goToTheMenu);
-$('#add').on('click', goToTheMenu);
-$('#cancel').on('click', goToTheMenu);
-
-
-
-$('#registration').click(function() { 
-    window.location.href = 'Registration.html'; 
-    return false; 
-}); 
-
-$('#addNewUser').on('click',function() { 
-    window.location.href = 'SignIn.html'; 
-    return false;  
-});
-
-$('#billingPeriod').click(function() {
-
-    ($('.dropdownContent').css('display') == 'none') ? $('.dropdownContent').css('display', 'block') : $('.dropdownContent').css('display', 'none');
+    ($('.dropdown-menu').css('display') == 'none') ? $('.dropdown-menu').css('display', 'block') : $('.dropdown-menu').css('display', 'none');
     
 });
